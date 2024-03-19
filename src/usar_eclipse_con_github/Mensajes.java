084b5796fc05fc5f033c8212576132890dfd70d6
AbelMoro <abel.moro@educa.madrid.org>
2024-03-19 22:07:40 +0100

    commit #3 dev branch

--- src/usar_eclipse_con_github/Mensajes.java
+++ src/usar_eclipse_con_github/Mensajes.java
@@ -5,4 +5,9 @@ public class Mensajes {
 	public static void commit1() {
 		System.out.println("Soy la funcion del primer commit en la rama development.");
 	}
+	
+	public static void commit3() {
+		System.out.println("Soy la funcion del tercer commit en la rama development.");
+	}
+
 }
